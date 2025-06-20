c3b0815b285d2a2473dbad4ac242541216afbde4
Yusuftask <yusuftaskiran1@gmail.com>
2025-06-20 08:20:05 +0300

    redeploy

--- app/layout.js
+++ app/layout.js
@@ -66,6 +66,8 @@ export default function RootLayout({ children }) {
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
